7294c21cb3f70d4ae839ac483332307d2d4b0b17
mohcine-otmane-youcode <moyoucode@outlook.com>
2025-09-02 14:28:46 +0100

    Challenge 5 completed

---
+++ Day_2/challenge5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+//Trouver le Maximum
+int main(int argc, char *argv[]) {
+	
+	int arrSize,i,j, somme=0,min;
+	
+	printf("Entrer la taille de Tableau\n");
+	scanf("%d",&arrSize);
+	
+	int arr[arrSize];
+	
+	for(i=0;i<arrSize;i++){
+		printf("[%d] = ",i);
+		scanf("%d",&arr[i]);
+		printf("\n");
+	}
+	
+	
+	for(i=0;i<arrSize;i++){
+		min = arr[i];
+		for(j=0;j<arrSize;j++){
+			if(min>arr[j]){
+				min=arr[j];
+			}
+		}
+	}
+	
+	printf("Min = %d",min);
+	
+	return 0;
+}
