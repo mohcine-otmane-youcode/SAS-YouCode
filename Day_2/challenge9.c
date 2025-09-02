1fcadde51bd175f6ae7d7298d76f4c8024ba6e63
mohcine-otmane-youcode <moyoucode@outlook.com>
2025-09-02 15:18:46 +0100

    Challenge 9 completed

---
+++ Day_2/challenge9.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+// Inversion d'un Tableau
+int main(int argc, char *argv[]) {
+	
+	int arrSize,i,j,a;
+	
+	printf("Entrer la taille de Tableau\n");
+	scanf("%d",&arrSize);
+	
+	int arr[arrSize];
+	int arrcopy[arrSize];
+	
+	for(i=0;i<arrSize;i++){
+		printf("[%d] = ",i);
+		scanf("%d",&arr[i]);
+		printf("\n");
+	}
+	
+	for(i=0;i<arrSize;i++){
+		arrcopy[i] = arr[arrSize-i-1];
+	}
+	
+	printf("---Tableau Original---\n");
+	for(i=0;i<arrSize;i++){
+		printf("[%d] = %d\t",i,arr[i]);
+	}
+	printf("\n");
+	printf("---Tableau Inverse---\n");
+	for(i=0;i<arrSize;i++){
+		printf("[%d] = %d\t",i,arrcopy[i]);
+	}
+	
+	
+	return 0;
+}
