ae8ce44f943f391409fbaafcce3105849a940b61
mohcine-otmane-youcode <moyoucode@outlook.com>
2025-09-03 15:52:42 +0100

    Mini projet en train d'etre complet

--- Day_2/Fonctions/Fonctions.c
+++ Day_2/Fonctions/Fonctions.c
@@ -23,10 +23,33 @@ void max(int a,int b){
 	}
 }
 
+void min(int a,int b){
+	//Fonction de Min
+	if(a<b){
+		printf("%d est le minimum (parmi %d et %d)\n",a,a,b);
+	} else {
+		printf("%d est le minimum (parmi %d et %d)\n",b,a,b);
+	}
+}
+
+int fact(int n){
+	return n*fact(n-1);
+}
+
 
 int main(int argc, char *argv[]) {
+	printf("Choisir l'opiration\n");
+	printf("1: Somme\n");
+	printf("2: Multiplication\n");
+	printf("3: Maximum\n");
+	printf("4: Minimum\n");
+	printf("5: Factorielle\n");
+	int n;
 	somme(8,5);
 	multip(8,5);
 	max(9,7);
+	min(9,7);
+	
+	printf("%d! = %d",n,fact(n));
 	return 0;
 }
---
+++ Mini_Projet/Mini_Projet.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char livresTitres[10][10];
+char livresAuteurs[10][10];
+float livresPrix[10];
+int stock=0,i,j;
+int livreId, attribu;
+
+
+
+
+
+
+int main(int argc, char *argv[]) {
+	int chois;
+	
+	
+	Les_chois:
+	system("cls");
+	// Affichage de chois
+	printf("=======================================\n");
+	printf("\x1b[32m1: Ajouter un Livre au Stock\n");
+	printf("\x1b[0m2: Afficher Tous les Livres Disponibles\n");
+	printf("3: Mettre a Jour la Quantite d'un Livre\n");
+	printf("\x1b[31m4: Supprimer un Livre du Stock\n");
+	printf("\x1b[0m5: Afficher le Nombre Total de Livres en Stock\n");
+	printf("0: Sortir\n");
+	printf("=======================================\n");
+	printf("Votre chois: \n");
+	scanf("%d",&chois);
+	system("cls");
+	
+	
+	switch(chois){
+		
+		case 1: //Ajouter
+			printf("Entrer le titre de livre\n");
+			scanf(" %[^\n]",&livresTitres[stock]);
+			printf("Entrer l\'auteur de livre\n");
+			scanf(" %[^\n]",&livresAuteurs[stock]);
+			printf("Entrer le prix de livre\n");
+			scanf("%f",&livresPrix[stock]);
+			stock++;
+		    goto Les_chois;
+		    
+		case 2: //Afficher
+			printf("Les livres disponibles sont\n");
+			for(i=0;i<stock;i++){
+				printf("---<== Livre: %d ==>---\n",i+1);
+				printf("Titre: %s\n",livresTitres[i]);
+				printf("Auteur: %s\n",livresAuteurs[i]);
+				printf("Prix: %f\n",livresPrix[i]);
+			}
+			printf("\n");
+			printf("1: Menu principale\n");
+			printf("0: sortir\n");
+			scanf("%d",&chois);
+			if(chois==0){
+				break;
+			} else {
+				goto Les_chois;
+			}
+			
+		    
+		    
+		case 3:
+			printf("Choisir le livre a mettre a jour\n");
+			scanf("%d",&livreId);
+			printf("Choisir l\'atribu a mettre a jours\n");
+			printf("1: Titre\n");
+			printf("2: Auteur\n");
+			printf("3: Prix\n");
+			scanf("%d",&attribu);
+			switch(attribu){
+				case 1:
+					printf("Entrer le titre de livre\n");
+					scanf(" %[^\n]",&livresTitres[livreId]);
+				case 2:
+					printf("Entrer l\'auteur de livre\n");
+					scanf(" %[^\n]",&livresAuteurs[livreId]);
+				case 3:
+					printf("Entrer le prix de livre\n");
+					scanf("%f",&livresPrix[livreId]);
+			}
+		    goto Les_chois;
+		case 4:
+//			supprimer_un_livre();
+			
+			system("cls");
+			
+			//Afficher
+			
+			afficher:
+			
+			printf("Les livres disponibles sont\n");
+			for(i=0;i<stock;i++){
+				printf("---<== Livre: %d ==>---\n",i+1);
+				printf("Titre: %s\n",livresTitres[i]);
+				printf("Auteur: %s\n",livresAuteurs[i]);
+				printf("Prix: %f\n",livresPrix[i]);
+			}
+			printf("\n");
+			
+			//Suprimer
+			printf("Choisir le livre a suprimer ou enter -1 pour annuler\n");
+			scanf("%d",&livreId);
+			if(livreId==-1){
+				goto Les_chois;
+			}
+			if(livreId>-1 && livreId<stock){
+				for(i=livreId;i<stock;i++){
+					for(j=0;j<10;j++){
+						livresTitres[i][j] = livresTitres[i+1][j];
+						livresTitres[i+1][j] = "";
+						livresAuteurs[i][j] = livresAuteurs[i+1][j];
+						livresAuteurs[i+1][j] = "";
+						livresPrix[i] = livresPrix[i+1];
+						livresPrix[i+1] = 0;
+					}
+				}
+//				stock--;
+			}
+			
+			
+			goto afficher;
+			
+//			livresTitres[livreId] = "";
+//			livresAuteurs[livreId] = "";
+			
+		    goto Les_chois;
+		case 5:
+//			afficher_le_nombre_total();
+			printf("Le nombre total de stock est: %d",stock);
+		    goto Les_chois;
+		case 6:
+		    break;
+	}
+	
+	
+	
+	return 0;
+}
