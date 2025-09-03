4bbc242ebd1b8dca5d3ebc199c7562917a3c7632
mohcine-otmane-youcode <moyoucode@outlook.com>
2025-09-03 11:41:05 +0100

    Fonction de maximum

--- Day_2/Fonctions/Fonctions.c
+++ Day_2/Fonctions/Fonctions.c
@@ -13,8 +13,20 @@ void multip(int a,int b){
 	printf("%d * %d = %d\n",a,b,a*b);
 }
 
+
+void max(int a,int b){
+	//Fonction de Max
+	if(a>b){
+		printf("%d est le maximum (parmi %d et %d)\n",a,a,b);
+	} else {
+		printf("%d est le maximum (parmi %d et %d)\n",b,a,b);
+	}
+}
+
+
 int main(int argc, char *argv[]) {
 	somme(8,5);
 	multip(8,5);
+	max(9,7);
 	return 0;
 }
