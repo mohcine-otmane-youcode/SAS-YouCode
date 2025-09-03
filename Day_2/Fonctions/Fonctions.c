725e3558ef5038d8da2ff94dbbf07818245eeee8
mohcine-otmane-youcode <moyoucode@outlook.com>
2025-09-03 11:37:06 +0100

    Fonction de multiplication

--- Day_2/Fonctions/Fonctions.c
+++ Day_2/Fonctions/Fonctions.c
@@ -5,10 +5,16 @@
 
 void somme(int a,int b){
 	//Fonction de Somme
-	printf("%d + %d = %d",a,b,a+b);
+	printf("%d + %d = %d\n",a,b,a+b);
+}
+
+void multip(int a,int b){
+	//Fonction de Multiplication
+	printf("%d * %d = %d\n",a,b,a*b);
 }
 
 int main(int argc, char *argv[]) {
 	somme(8,5);
+	multip(8,5);
 	return 0;
 }
