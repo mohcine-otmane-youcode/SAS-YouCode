7ec433961738bbc1c29d49653f3fe1ddebb329de
mohcine-otmane-youcode <moyoucode@outlook.com>
2025-09-05 20:25:16 +0100

    Mini projet 2 comencement

--- Mini_Projet/Mini_Projet.c
+++ Mini_Projet/Mini_Projet.c
@@ -4,18 +4,17 @@
 #include <unistd.h>
 
 
-
 char livresTitres[10][10], livresAuteurs[10][10], titre[10];
 float livresPrix[10], livresQuantite[10];
 int stock=0,i,j, sizeTitreMax = 2, sizeAuteurMax = 6, livreId, attribu, choix;
 
 
 
-int main(int argc, char *argv[]) {
+int main() {
 
-		
+    system("cls");
 	Les_choix:
-		// Affichage de chois
+		// Affichage de choix
 		printf("\t\t\t\t\x1b[104m                                                           \x1b[0m\n");
 		printf("\t\t\t\t\x1b[45m \x1b[0m	\x1b[5;32;7;49m1: Ajouter un livre\x1b[0m                               \x1b[45m \x1b[0m\n");
 		printf("\t\t\t\t\x1b[45m \x1b[0m	2: Afficher Tous les Livres Disponibles           \x1b[45m \x1b[0m\n");
@@ -25,7 +24,7 @@ int main(int argc, char *argv[]) {
 		printf("\t\t\t\t\x1b[45m \x1b[0m	\x1b[0m6: Nombre Total de Livres en Stock                \x1b[45m \x1b[0m\n");
 		printf("\t\t\t\t\x1b[45m \x1b[0m	0: Sortir                                         \x1b[45m \x1b[0m\n");
 		printf("\t\t\t\t\x1b[104m                                                           \x1b[0m\n");
-		
+
 		printf("\x1b[5;32;7;49m->\x1b[0m:");
 		scanf("%d",&choix);
 		if(choix==1){
@@ -45,18 +44,18 @@ int main(int argc, char *argv[]) {
 			goto Suprimer;
 		} else if(choix==6){
 			system("cls");
-//			goto Nombrestock;
+			goto Nombrestock;
 		} else {
 			printf("\t\t\t\t	\x1b[5;31;3;49m Erreur, Vous avez abuse le systeme\x1b[0m                    \n");
 			exit(1);
 		}
-	
+
 	Ajouter:
 			system("cls");
 			printf("\t\t\t\t\x1b[104m                                                           \x1b[0m\n");
 			printf("\t\t\t\t\x1b[45m \x1b[0m	\x1b[5;32;5;49m           Ajouter un livre                       \x1b[45m \x1b[0m\n");
 			printf("\t\t\t\t\x1b[104m                                                           \x1b[0m\n");
-			
+
 			printf("\n\n");
 			printf("\x1b[42m    \x1b[0mEntrer le titre de livre\x1b[42m    \x1b[0m\n");
 			scanf(" %[^\n]",&livresTitres[stock]);
@@ -70,97 +69,97 @@ int main(int argc, char *argv[]) {
 				printf("\x1b[42m    \x1b[0mEntrer le prix de livre\x1b[42m    \x1b[0m\n");
 				scanf("%f",&prix);
 			}
-			
+
 			livresPrix[i] = prix;
-			
-			
-			
-			
+
+
+
+
 			printf("\x1b[42m    \x1b[0mEntrer la quantite\x1b[42m    \x1b[0m\n");
 			scanf("%f",&livresQuantite[stock]);
 			stock++;
-			
+
 			printf("\x1b[32mLivre Ajoute avec succes entrer 1 pour revenir\x1b[0m\n");
-			
+
 			if(scanf("%d",&attribu)==1){
 				system("cls");
 				goto Les_choix;
 			}
-			
-		    
-		    
-	
+
+
+
+
 	Afficher:
 			//Titres de collones
 			printf("Les livres disponibles sont\n");
 			//ID
-			printf("\t\t\t\t\x1b[104m+");
+			printf("\t\t\t\t\x1b[104m ");
 			for(j=0;j<2+4;j++){
-				printf("-");
+				printf(" ");
 			}
 			//Titre
-			printf("+");
+			printf(" ");
 			for(j=0;j<5+4;j++){
-				printf("-");
+				printf(" ");
 			}
 			//Auteur
-			printf("+");
+			printf(" ");
 			for(j=0;j<6+4;j++){
-				printf("-");
+				printf(" ");
 			}
-			
+
 			//Prix
-			printf("+");
+			printf(" ");
 			for(j=0;j<4+4;j++){
-				printf("-");
+				printf(" ");
 			}
 			//Quantite
-			printf("+");
+			printf(" ");
 			for(j=0;j<8+4;j++){
-				printf("-");
+				printf(" ");
 			}
-			printf("+\x1b[0m\n");
-			
-			
+			printf(" \x1b[0m\n");
+
+
 			printf("\t\t\t\t|  ID  |  Titre  |  Auteur  |  Prix  |  Quantite  |\n");
-			
-			
+
+
 			//ID
-			printf("\t\t\t\t\x1b[104m+");
+			printf("\t\t\t\t\x1b[104m ");
 			for(j=0;j<2+4;j++){
-				printf("-");
+				printf(" ");
 			}
 			//Titre
-			printf("+");
+			printf(" ");
 			for(j=0;j<5+4;j++){
-				printf("-");
+				printf(" ");
 			}
 			//Auteur
-			printf("+");
+			printf(" ");
 			for(j=0;j<6+4;j++){
-				printf("-");
+				printf(" ");
 			}
-			
+
 			//Prix
-			printf("+");
+			printf(" ");
 			for(j=0;j<4+4;j++){
-				printf("-");
+				printf(" ");
 			}
 			//Quantite
-			printf("+");
+			printf(" ");
 			for(j=0;j<8+4;j++){
-				printf("-");
+				printf(" ");
 			}
-			printf("+\x1b[0m");
-		
-			
+			printf(" \x1b[0m");
+
+
 			for(i=0;i<stock;i++){
 				printf("\n");
 				printf("\t\t\t\t|  %.2d  |", i+1, livresTitres[i],livresAuteurs[i]);
 				printf("  %.5s  |", livresTitres[i]);
-				
+
 				printf("  %.6s  ", livresAuteurs[i]);
-				
+
 				printf("|  %.2f ", livresPrix[i]);
 				if(livresPrix[i]<10){
 					printf(" ");
@@ -170,50 +169,50 @@ int main(int argc, char *argv[]) {
 					printf(" ");
 				}
 				printf("|\n");
-			
+
 				//ID
-				printf("\t\t\t\t\x1b[104m+");
+				printf("\t\t\t\t\x1b[104m ");
 				for(j=0;j<2+4;j++){
-					printf("-");
+					printf(" ");
 				}
 				//Titre
-				printf("+");
+				printf(" ");
 				for(j=0;j<5+4;j++){
-					printf("-");
+					printf(" ");
 				}
 				//Auteur
-				printf("+");
+				printf(" ");
 				for(j=0;j<6+4;j++){
-					printf("-");
+					printf(" ");
 				}
-				
+
 				//Prix
-				printf("+");
+				printf(" ");
 				for(j=0;j<4+4;j++){
-					printf("-");
+					printf(" ");
 				}
 				//Quantite
-				printf("+");
+				printf(" ");
 				for(j=0;j<8+4;j++){
-					printf("-");
+					printf(" ");
 				}
-				printf("+\x1b[0m");
+				printf(" \x1b[0m");
 			}
-				
+
 			printf("\x1b[0m\n");
-		
-				
+
+
 	        printf("Entrer 1 pour revenir au menu principal\n");
 	        if(scanf("%d",&attribu)==1){
 	        	system("cls");
 	            goto Les_choix;
 	        }
-        
-        
+
+
         Mettre_a_jour:
-        	
+
         	printf("\n\n");
-        	
+
         	if(stock>0){
         		system("cls");
 				printf("\t\t\t\t\x1b[104m                                                           \x1b[0m\n");
@@ -225,7 +224,7 @@ int main(int argc, char *argv[]) {
 					printf("Auteur: %s\n",livresAuteurs[i]);
 					printf("Prix: %f\n",livresPrix[i]);
 				}
-	
+
 				printf("Choisir le livre a mettre a jour\n");
 				scanf("%d",&livreId);
 				printf("Choisir l\'atribu a mettre a jours\n");
@@ -233,7 +232,7 @@ int main(int argc, char *argv[]) {
 				printf("2: Auteur\n");
 				printf("3: Prix\n");
 				scanf("%d",&attribu);
-	
+
 				switch(attribu){
 					case 1:
 						printf("Entrer le titre de livre\n");
@@ -254,24 +253,24 @@ int main(int argc, char *argv[]) {
 				printf("\t\t\t\t\x1b[104m                                                           \x1b[0m\n");
 				printf("\t\t\t\t\x1b[45m \x1b[0m	\x1b[5;32;49m           Pas de Livres disponibles              \x1b[45m \x1b[0m\n");
 				printf("\t\t\t\t\x1b[104m                                                           \x1b[0m\n");
-				
+
 				printf("Clicker Entrer pour revenir au menu principal\n");
 				getchar();
 				getchar();
 			}
 			system("cls");
 			goto Les_choix;
-	
-		
-		
+
+
+
 		Rechercher:
-			
+
 			system("cls");
 			printf("\t\t\t\x1b[104m                                                           \x1b[0m\n");
 			printf("\t\t\t\x1b[45m \x1b[0m	\x1b[5;32;5;49m           Rechercher un livre                    \x1b[45m \x1b[0m\n");
 			printf("\t\t\t\x1b[104m                                                           \x1b[0m\n");
-			
-			
+
+
 			printf("\n\n");
 			printf("\x1b[42m    \x1b[0mChercher un livre par\x1b[42m    \x1b[0m\n");
 			printf("\n\n");
@@ -283,8 +282,8 @@ int main(int argc, char *argv[]) {
                 printf("Le titre a chercher: ");
                 scanf(" %[^\n]",&titre);
                 for(i=0;i<stock;i++){
-                	                    
-                    if(strcmp(livresTitres[i],titre)==0){									                	                    
+
+                    if(strcmp(livresTitres[i],titre)==0){
                     	printf("Le livre que vous cherchez\n");
                       	printf("Titre: %s\n",livresTitres[i]);
                       	printf("Auteur: %s\n",livresAuteurs[i]);
@@ -296,13 +295,13 @@ int main(int argc, char *argv[]) {
                 }
             }
             printf("Entrer 0 pour revenir au menu principal\n");
-            
+
 	        scanf("%d",&attribu);
 	        if(attribu==0){
 	            goto Les_choix;
 	        }
-          
-            
+
+
         Suprimer:
         	goto Afficher;
 
@@ -325,12 +324,12 @@ int main(int argc, char *argv[]) {
 				printf("Choix incorrect\n");
 			}
 		    goto Les_choix;
-            
-        
+
+
         Nombrestock:
         	printf("Le nombre total de stock est: %d",stock);
 		    goto Les_choix;
-		    
+
 	return 0;
 
 }
--- Mini_Projet_2/Mini_Projet_2.c
+++ Mini_Projet_2/Mini_Projet_2.c
@@ -1,14 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int num_contact = 0,i,j;
+
+struct Contact{
+	char Nom[10];
+	char Numero[10];
+	char Email[10];
+};
+
+struct Contact contacts[100];
+
+void  Ajouter_un_Contact(){
+	if(num_contact<100){
+		struct Contact contact;
+		printf("Ajouter un contact\n");
+		printf("Nom de contact: ");
+		scanf("%s",&contact.Nom);
+		printf("\n");
+		printf("Numero de telephone: ");
+		scanf("%s",&contact.Numero);
+		printf("\n");
+		printf("Email: ");
+		scanf("%s",&contact.Email);
+		printf("\n");
+		contacts[num_contact] = contact;
+		num_contact++;
+	} else {
+		printf("Le livre de contacts est plain\n");
+	}
+}
+
+void Afficher_Un_Contact(int contactId){
+	int nomMax = 3, telMax = 9, emailMax = 5;
+    for (int i = 0; i < num_contact; i++) {
+        int nomLen = strlen(contacts[i].Nom);
+        int numlen = strlen(contacts[i].Numero);
+        int emailLen = strlen(contacts[i].Email);
+        if (nomLen > nomMax){
+			nomMax = nomLen;
+		}
+        if (numlen > telMax){
+			telMax = numlen;
+		}
+        if (emailLen > emailMax){ 
+			emailMax = emailLen;
+		}
+    }
+	printf("| %-2d | %-*s | %-*s | %-*s |\n",contactId + 1, nomMax, contacts[contactId].Nom,telMax, contacts[contactId].Numero,emailMax, contacts[contactId].Email);
+}
+
+void Afficher_Contacts(){
+	for (int i = 0; i < num_contact; i++) {
+		Afficher_Un_Contact(i);
+	}
+}
+
+
+void Rechercher_un_Contact(){
+	char Nom[10];
+	system("cls");
+	printf("\t\t\t\t\x1b[104m                                                           \x1b[0m\n");
+	printf("\t\t\t\t\x1b[45m \x1b[0m	\x1b[5;32;5;49m               Recherche                          \x1b[45m \x1b[0m\n");
+	printf("\t\t\t\t\x1b[104m                                                           \x1b[0m\n");
+	printf("\n\n");
+	printf("\x1b[42m    \x1b[0mEntrer le nom de contact\x1b[42m    \x1b[0m\n");
+	scanf(" %[^\n]",&Nom);
+
+	for(i=0;i<num_contact;i++){
+		if(strcmp(Nom,contacts[i].Nom)==0){
+			// Afficher_Un_Contact(i);
+		}
+	}
+
+
+}
+
+void Supprimer_un_Contact(){
+	
+}
 
 
 
-
-
-
+int Afficher_Choix(){
+	// Affichage de choix
+		int choix;
+		printf("\t\t\t\t\x1b[104m                                                           \x1b[0m\n");
+		printf("\t\t\t\t\x1b[45m \x1b[0m	\x1b[6;32;7;49m1: Ajouter un Contact\x1b[0m                             \x1b[45m \x1b[0m\n");
+		printf("\t\t\t\t\x1b[45m \x1b[0m	2: Afficher Tous les Contacts Disponibles         \x1b[45m \x1b[0m\n");
+		printf("\t\t\t\t\x1b[45m \x1b[0m	3: Mettre un contact                              \x1b[45m \x1b[0m\n");
+		printf("\t\t\t\t\x1b[45m \x1b[0m	4: Rechercher un Livre                            \x1b[45m \x1b[0m\n");
+		printf("\t\t\t\t\x1b[45m \x1b[0m	\x1b[5;31;7;49m5: Supprimer un contact\x1b[0m                           \x1b[45m \x1b[0m\n");
+		printf("\t\t\t\t\x1b[45m \x1b[0m	0: Sortir                                         \x1b[45m \x1b[0m\n");
+		printf("\t\t\t\t\x1b[104m                                                           \x1b[0m\n");
+		return choix;
+}
 
 int main(){
-	
+	int choix;
+	// while(1==1){
+	// 	choix = Afficher_Choix();
+	// 	Ajouter_un_Contact();
+	// 	Afficher_Tous_les_Contacts();
+		
+	// }
+
+	Ajouter_un_Contact();
+	Ajouter_un_Contact();
+	Afficher_Contacts();
+	// Rechercher_un_Contact();
 	
 	
 	
